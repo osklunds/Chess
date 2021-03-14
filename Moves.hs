e224c47ee4b637837cab73c6923cf79377f98feb
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2021-03-14 18:10:57 +0100

    Improve readability Moves

--- Moves.hs
+++ Moves.hs
@@ -7,125 +7,124 @@ where
 import Board as B
 
 movesForColor :: Color -> Board -> [((Int,Int),(Int,Int))]
-movesForColor color board = concat [movesAtPos (row,col) color board |
+movesForColor color board = concat [movesFromPos (row,col) color board |
                                     row <- [0..7], col <- [0..7]]
 
-movesAtPos :: (Int,Int) -> Color -> Board -> [((Int,Int),(Int,Int))]
-movesAtPos pos c board
-  | B.isColor c atPos = movesFun pos board
-  | otherwise = []
+movesFromPos :: (Int,Int) -> Color -> Board -> [((Int,Int),(Int,Int))]
+movesFromPos pos color board
+  | isColor color atPos = movesFun pos board
+  | otherwise           = []
   where
-    atPos = B.get pos board
+    atPos = get pos board
     movesFun = case atPos of
-                 (Piece _ Queen)  -> queenMoves
                  (Piece _ King )  -> kingMoves
-                 (Piece _ Bishop) -> bishopMoves
+                 (Piece _ Queen)  -> queenMoves
                  (Piece _ Rook)   -> rookMoves
-                 (Piece _ Pawn)   -> pawnMoves
+                 (Piece _ Bishop) -> bishopMoves
                  (Piece _ Knight) -> knightMoves
-                 Empty            -> \_ _ -> []
+                 (Piece _ Pawn)   -> pawnMoves
 
 kingMoves :: (Int,Int) -> Board -> [((Int,Int),(Int,Int))]
-kingMoves pos board = filter f $ queenMoves pos board
+kingMoves pos board = filter isOneStep $ queenMoves pos board
   where
-    f (start,dest) = tupleMaxAbs (dest `tupleSub` start) <= 1
+    isOneStep (start,dest) = tupleMaxAbs (dest `tupleSub` start) <= 1
 
 queenMoves :: (Int,Int) -> Board -> [((Int,Int),(Int,Int))]
-queenMoves pos board = concatMap movesFun (straightDeltas ++ diagonalDeltas)
-  where
-    atPos    = get pos board
-    color    = B.color atPos
-    movesFun = (\d -> movesFromDelta board d pos color)
-
-bishopMoves :: (Int,Int) -> Board -> [((Int,Int),(Int,Int))]
-bishopMoves pos board = concatMap movesFun diagonalDeltas
-  where
-    atPos      = get pos board
-    color      = B.color atPos
-    movesFun d = movesFromDelta board d pos color
+queenMoves = movesFromDirs queenDirs
 
 rookMoves :: (Int,Int) -> Board -> [((Int,Int),(Int,Int))]
-rookMoves pos board = concatMap movesFun straightDeltas
+rookMoves = movesFromDirs rookDirs
+
+bishopMoves :: (Int,Int) -> Board -> [((Int,Int),(Int,Int))]
+bishopMoves = movesFromDirs bishopDirs
+
+movesFromDirs :: [(Int,Int)] -> (Int,Int) -> Board -> [((Int,Int),(Int,Int))]
+movesFromDirs dirs start board = concatMap movesFun dirs
   where
-    atPos      = get pos board
-    color      = B.color atPos
-    movesFun d = movesFromDelta board d pos color
+    atPos         = get start board
+    color         = B.color atPos
+    movesFun diff = movesFromColorAndDiff color diff start board
+
+movesFromColorAndDiff :: Color ->
+                         (Int,Int) ->
+                         (Int,Int) ->
+                         Board ->
+                         [((Int,Int),(Int,Int))]
+movesFromColorAndDiff color diff start board
+  | not (isWithinBoard dest)  = []
+  | isEmpty atDest            = (thisMove:remainingMoves)
+  | isOtherColor color atDest = [thisMove]
+  | otherwise                 = []
+  where
+    dest           = start `tupleAdd` diff
+    atDest         = get dest board
+    thisMove       = (start,dest)
+    remainingMoves = movesFromColorAndDiff color nextDiff start board
+    nextDiff       = diff `tupleAdd` tupleSignum diff
+
+queenDirs :: [(Int,Int)]
+queenDirs = rookDirs ++ bishopDirs
+
+rookDirs :: [(Int,Int)]
+rookDirs = [(1,0),  -- Down
+            (0,-1), -- Left
+            (-1,0), -- Up
+            (0,1)]  -- Right
+
+bishopDirs :: [(Int,Int)]
+bishopDirs = [(1,1),   -- Down-right
+              (1,-1),  -- Down-left
+              (-1,-1), -- Up-left
+              (-1,1)]  -- Up-right
+
+knightMoves :: (Int,Int) -> Board -> [((Int,Int),(Int,Int))]
+knightMoves start board = filter hasValidDest moves
+  where
+    (Piece color Knight) = get start board
+    moves                = map (\diff -> (start, start `tupleAdd` diff))
+                               knightDiffs
+    hasValidDest (_start,dest) = let atDest = get dest board
+                                 in  isWithinBoard dest &&
+                                     not (isColor color atDest)
+
+knightDiffs = [(2,1),  -- L
+               (2,-1),
+               (1,-2),
+               (-1,-2),
+               (-2,-1),
+               (-2,1),
+               (1,2),
+               (-1,2)]
 
 pawnMoves :: (Int,Int) -> Board -> [((Int,Int),(Int,Int))]
-pawnMoves pos board = [(pos,dest) | dest <- dests]
+pawnMoves start board = [(start,dest) | dest <- dests]
   where
-    atPos       = get pos board
-    color       = B.color atPos
-    delta       = case color of
+    color       = B.color $ get start board
+    forwardDir  = case color of
                     Black -> 1
                     White -> (-1)
-
-    posCenter  = pos `tupleAdd` (delta,0)
-    posDouble  = pos `tupleAdd` (delta*2,0)
-    posLeft    = pos `tupleAdd` (delta,-1)
-    posRight   = pos `tupleAdd` (delta,1)
-
-    atCenter    = get posCenter board
-    atDouble    = get posDouble board
-    atLeft      = get posLeft  board
-    atRight     = get posRight board
-
-    (row,_col)  = pos
+    (row,_col)  = start
     isAtInitial = case color of
                     Black -> row == 1
                     White -> row == 6
 
-    dests     = case isWithinBoard posLeft && isOtherColor color atLeft of
-                  True  -> [posLeft]
-                  False -> []
-                ++
-                case isWithinBoard posRight && isOtherColor color atRight of
-                  True  -> [posRight]
-                  False -> []
-                ++
-                case isWithinBoard posCenter && isEmpty atCenter of
-                  True ->  [posCenter]
-                  False -> []
-                ++
-                case isWithinBoard posDouble && isEmpty atCenter &&
-                     isEmpty atDouble && isAtInitial of
-                  True -> [posDouble]
-                  False -> []
+    left          = start `tupleAdd` (forwardDir, -1)
+    right         = start `tupleAdd` (forwardDir,  1)
+    forward       = start `tupleAdd` (forwardDir,  0)
+    doubleForward = start `tupleAdd` (forwardDir*2,0)
 
--- pos -> start
-knightMoves :: (Int,Int) -> Board -> [((Int,Int),(Int,Int))]
-knightMoves pos board = filter hasValidDest moves
-  where
-    atPos    = get pos board
-    color    = B.color atPos
-    diffs    = [(2,1),  -- L
-                (2,-1),
-                (1,-2),
-                (-1,-2),
-                (-2,-1),
-                (-2,1),
-                (1,2),
-                (-1,2)]
-    moves    = map (\diff -> (pos, pos `tupleAdd` diff)) diffs
-    hasValidDest (_start,dest) = isWithinBoard dest &&
-                                 not (isColor color atDest)
-      where
-        atDest = get dest board
+    atLeft          = get left  board
+    atRight         = get right board
+    atForward       = get forward board
+    atDoubleForward = get doubleForward board
 
-
-
--- deltas -> dir
-straightDeltas :: [(Int,Int)]
-straightDeltas = [(1,0),  -- Down
-                  (0,-1), -- Left
-                  (-1,0), -- Up
-                  (0,1)]  -- Right
-
-diagonalDeltas :: [(Int,Int)]
-diagonalDeltas = [(1,1),   -- Down-right
-                  (1,-1),  -- Down-left
-                  (-1,-1), -- Up-left
-                  (-1,1)]  -- Up-right
+    dests = [left    | isWithinBoard left    && isOtherColor color atLeft] ++
+            [right   | isWithinBoard right   && isOtherColor color atRight] ++
+            [forward | isWithinBoard forward && isEmpty atForward] ++
+            [doubleForward | isWithinBoard doubleForward &&
+                             isEmpty atDoubleForward &&
+                             isEmpty atForward &&
+                             isAtInitial]
 
 tupleAdd :: (Int,Int) -> (Int,Int) -> (Int,Int)
 tupleAdd (a,b) (c,d) = (a+c,b+d)
@@ -141,21 +140,3 @@ tupleMaxAbs (a,b) = max (abs a) (abs b)
 
 isWithinBoard :: (Int,Int) -> Bool
 isWithinBoard (row,col) = 0 <= row && row < 8 && 0 <= col && col < 8
-
--- delta -> dir
-movesFromDelta :: Board ->
-                  (Int,Int) ->
-                  (Int,Int) ->
-                  Color ->
-                  [((Int,Int),(Int,Int))]
-movesFromDelta board diff start color
-  | not (isWithinBoard pos)    = []
-  | B.isEmpty atPos            = (here:rest)
-  | B.isOtherColor color atPos = [here]
-  | otherwise                  = []
-  where
-    pos   = start `tupleAdd` diff
-    atPos = B.get pos board
-    here  = (start,pos) -- here -> currMove
-    rest  = movesFromDelta board diff' start color
-    diff' = diff `tupleAdd` tupleSignum diff
