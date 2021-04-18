e708cb8fb96a05ec6850f67fceb14bfbd470b82c
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2021-04-18 13:36:38 +0200

    Implement and test check aware moves

--- src/Moves.hs
+++ src/Moves.hs
@@ -4,10 +4,23 @@ module Moves
 )
 where
 
+import Prelude as P
+
 import Board as B
 import qualified Moves.CheckUnawareMoves as CU
 
 
 movesForColor :: Color -> Board -> [((Int,Int),(Int,Int))]
-movesForColor = CU.movesForColor
--- TODO: Take check into account
+movesForColor color board = filter (isKingSafeAfterMove color board) allMoves
+  where
+    allMoves               = CU.movesForColor color board
+
+isKingSafeAfterMove :: Color -> Board -> ((Int,Int),(Int,Int)) -> Bool
+isKingSafeAfterMove color board move = P.any (/= Piece color King) destSquares
+  where
+    newBoard    = applyMove move board
+    movesOther  = CU.movesForColor (otherColor color) newBoard
+    dests       = P.map snd movesOther
+    destSquares = P.map (\dest -> get dest newBoard) dests
+
+
