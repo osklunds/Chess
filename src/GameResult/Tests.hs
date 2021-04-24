d1329ec31e369655db559afe5af83b8b57c8c2ba
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2021-04-24 12:12:04 +0200

    Simplify boards for game result tests

--- src/GameResult/Tests.hs
+++ src/GameResult/Tests.hs
@@ -12,28 +12,28 @@ prop_resultNormal :: Bool
 prop_resultNormal = gameResult Black board == Normal
   where
     board = read  "  0 1 2 3 4 5 6 7  \n\
-                  \0 ♟             ♝ 0\n\
-                  \1   ♞           ♝ 1\n\
-                  \2   ♔     ♝       2\n\
-                  \3     ♜   ♙     ♙ 3\n\
-                  \4     ♛   ♖ ♟ ♗ ♚ 4\n\
-                  \5     ♖         ♜ 5\n\
-                  \6       ♘ ♞ ♟ ♝ ♟ 6\n\
-                  \7       ♜   ♞ ♖ ♝ 7\n\
+                  \0                 0\n\
+                  \1                 1\n\
+                  \2   ♔             2\n\
+                  \3                 3\n\
+                  \4                 4\n\
+                  \5             ♖   5\n\
+                  \6                 6\n\
+                  \7               ♚ 7\n\
                   \  0 1 2 3 4 5 6 7"
 
 prop_resultCheck :: Bool
 prop_resultCheck = gameResult Black board == Check
   where
     board = read  "  0 1 2 3 4 5 6 7  \n\
-                  \0 ♟             ♝ 0\n\
-                  \1   ♞           ♝ 1\n\
-                  \2   ♔     ♝       2\n\
-                  \3     ♜   ♙   ♗ ♙ 3\n\
-                  \4     ♛   ♖ ♟   ♚ 4\n\
-                  \5     ♖         ♜ 5\n\
-                  \6       ♘ ♞ ♟ ♝ ♟ 6\n\
-                  \7       ♜   ♞ ♖ ♝ 7\n\
+                  \0                 0\n\
+                  \1                 1\n\
+                  \2   ♔             2\n\
+                  \3                 3\n\
+                  \4                 4\n\
+                  \5               ♖ 5\n\
+                  \6                 6\n\
+                  \7               ♚ 7\n\
                   \  0 1 2 3 4 5 6 7"
 
 prop_resultCheckmate :: Bool
