4ef2950dce44b19890369befcbfb8a2159b7a507
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2021-04-24 13:01:00 +0200

    Add more tests to game result

--- src/GameResult/Tests.hs
+++ src/GameResult/Tests.hs
@@ -43,7 +43,21 @@ prop_resultCheckmate = gameResult Black board == Checkmate
                   \0                 0\n\
                   \1                 1\n\
                   \2   ♔             2\n\
-                  \3                 3\n\
+                  \3       ♟         3\n\
+                  \4                 4\n\
+                  \5             ♖ ♖ 5\n\
+                  \6                 6\n\
+                  \7               ♚ 7\n\
+                  \  0 1 2 3 4 5 6 7"
+
+prop_resultNotCheckmateCanCapture :: Bool
+prop_resultNotCheckmateCanCapture = gameResult Black board == Check
+  where
+    board = read  "  0 1 2 3 4 5 6 7  \n\
+                  \0                 0\n\
+                  \1                 1\n\
+                  \2   ♔             2\n\
+                  \3       ♟       ♜ 3\n\
                   \4                 4\n\
                   \5             ♖ ♖ 5\n\
                   \6                 6\n\
@@ -59,6 +73,20 @@ prop_resultDraw = gameResult Black board == Draw
                   \2   ♔             2\n\
                   \3                 3\n\
                   \4                 4\n\
+                  \5             ♖   5\n\
+                  \6             ♖   6\n\
+                  \7               ♚ 7\n\
+                  \  0 1 2 3 4 5 6 7"
+
+prop_resultNotDrawOtherCanMove :: Bool
+prop_resultNotDrawOtherCanMove = gameResult Black board == Normal
+  where
+    board = read  "  0 1 2 3 4 5 6 7  \n\
+                  \0                 0\n\
+                  \1                 1\n\
+                  \2   ♔             2\n\
+                  \3                 3\n\
+                  \4       ♟         4\n\
                   \5             ♖   5\n\
                   \6             ♖   6\n\
                   \7               ♚ 7\n\
