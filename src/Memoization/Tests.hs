963caa1d1c8f40a7e600069e3b0776f19f74cf78
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2022-09-11 10:32:15 +0200

    prop_eval_twice

--- src/Memoization/Tests.hs
+++ src/Memoization/Tests.hs
@@ -22,7 +22,28 @@ prop_eval arg = monadicIO $ do
 
     -- Assert
     assert $ memo1 /= memo2
-    assert $ ret == Return arg
+    assert $ ret == fun arg
+
+prop_eval_twice :: Int -> Int -> Property
+prop_eval_twice arg1 arg2 = arg1 /= arg2 ==> monadicIO $ do
+    -- Arrange
+    let memo1 = M.new fun
+    let (memo2, _ret1) = M.eval arg1 memo1
+
+    -- Act
+    let (memo3, ret2) = M.eval arg2 memo2
+
+    -- Assert
+    let (_memo4, ret3) = M.eval arg1 memo3
+
+    assert $ memo2 /= memo3
+    assert $ ret2 == fun arg2
+    assert $ ret3 == fun arg1
+
+
+
+
+
 
 fun arg = Return arg
 
