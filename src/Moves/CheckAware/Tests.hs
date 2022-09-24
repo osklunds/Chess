4b4e64266e61956bc05b31a0ce591b4cf7e8cb9d
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2022-09-24 11:32:56 +0200

    Make CheckAware.Tests use Pos diretly

--- src/Moves/CheckAware/Tests.hs
+++ src/Moves/CheckAware/Tests.hs
@@ -18,7 +18,7 @@ import Lib
 --------------------------------------------------------------------------------
 
 prop_fixedBoard1 :: Property
-prop_fixedBoard1 = verifyMoves moves Black board
+prop_fixedBoard1 = verifyMoves expMoves Black board
   where
     board = read  "  0 1 2 3 4 5 6 7  \n\
                   \0 ♟             ♝ 0\n\
@@ -30,60 +30,73 @@ prop_fixedBoard1 = verifyMoves moves Black board
                   \6       ♘ ♞ ♟ ♝ ♟ 6\n\
                   \7   ♟   ♜   ♞ ♖ ♝ 7\n\
                   \  0 1 2 3 4 5 6 7"
-    moves = -- King at (4,7)
-            (movesFrom (4,7) [(3,6), (4,6)]) ++
-            -- Note that (3,7) and (5,6) are missing as destinations
+    expMoves = -- King at (4 7)
+               (normalMovesFrom (Pos 4 7) [(Pos 3 6), (Pos 4 6)]) ++
+               -- Note that (Pos 3 7) and (Pos 5 6) are missing as destinations
 
-            -- Queen at (4,2)
-            (movesFrom (4,2) [(4,3), (4,4), (5,2), (5,1), (6,0), (4,1), (4,0),
-                              (3,1), (2,0), (3,3), (5,3)]) ++
+               -- Queen at (4 2)
+               (normalMovesFrom (Pos 4 2) [(Pos 4 3), (Pos 4 4), (Pos 5 2),
+                                           (Pos 5 1), (Pos 6 0), (Pos 4 1),
+                                           (Pos 4 0), (Pos 3 1), (Pos 2 0),
+                                           (Pos 3 3), (Pos 5 3)
+                                          ]) ++
 
-            -- Bishop at (0,7)
-            (movesFrom (0,7) [(1,6), (2,5), (3,4)]) ++
+               -- Bishop at (0 7)
+               (normalMovesFrom (Pos 0 7) [(Pos 1 6), (Pos 2 5), (Pos 3 4)]) ++
 
-            -- Bishop at (1,7)
-            (movesFrom (1,7) [(2,6), (3,5), (4,4), (0,6)]) ++
+               -- Bishop at (1 7)
+               (normalMovesFrom (Pos 1 7) [(Pos 2 6), (Pos 3 5), (Pos 4 4),
+                                           (Pos 0 6)
+                                          ]) ++
 
-            -- Bishop at (2,4)
-            (movesFrom (2,4) [(1,5), (0,6), (3,5), (4,6), (3,3), (1,3),
-                              (0,2)]) ++
+               -- Bishop at (2 4)
+               (normalMovesFrom (Pos 2 4) [(Pos 1 5), (Pos 0 6), (Pos 3 5),
+                                           (Pos 4 6), (Pos 3 3), (Pos 1 3),
+                                           (Pos 0 2)
+                                          ]) ++
 
-            -- Bishop at (6,6)
-            (movesFrom (6,6) [(5,5), (4,4)]) ++
+               -- Bishop at (6 6)
+               (normalMovesFrom (Pos 6 6) [(Pos 5 5), (Pos 4 4)]) ++
 
-            -- Pawn at (0,0)
-            (movesFrom (0,0) [(1,0)]) ++
+               -- Pawn at (0 0)
+               (normalMovesFrom (Pos 0 0) [(Pos 1 0)]) ++
 
-            -- Pawn at (6,5)
-            (movesFrom (6,5) [(7,6)]) ++
+               -- Pawn at (6 5)
+               (normalMovesFrom (Pos 6 5) [(Pos 7 6)]) ++
 
-            -- Pawn at (6,7)
-            (movesFrom (6,7) [(7,6)]) ++
+               -- Pawn at (6 7)
+               (normalMovesFrom (Pos 6 7) [(Pos 7 6)]) ++
 
-            -- Pawn at (4,5)
-            (movesFrom (4,5) [(5,5)]) ++
+               -- Pawn at (4 5)
+               (normalMovesFrom (Pos 4 5) [(Pos 5 5)]) ++
 
-            -- Rook at (7,3)
-            (movesFrom (7,3) [(7,4), (7,2), (6,3)]) ++
+               -- Rook at (7 3)
+               (normalMovesFrom (Pos 7 3) [(Pos 7 4), (Pos 7 2), (Pos 6 3)]) ++
 
-            -- Rook at (5,7)
-            (movesFrom (5,7) [(5,6), (5,5), (5,4), (5,3), (5,2)]) ++
+               -- Rook at (5 7)
+               (normalMovesFrom (Pos 5 7) [(Pos 5 6), (Pos 5 5), (Pos 5 4),
+                                           (Pos 5 3), (Pos 5 2)
+                                          ]) ++
 
-            -- Rook at (3,2)
-            (movesFrom (3,2) [(2,2), (1,2), (0,2), (3,3), (3,4), (3,1),
-                              (3,0)]) ++
+               -- Rook at (3 2)
+               (normalMovesFrom (Pos 3 2) [(Pos 2 2), (Pos 1 2), (Pos 0 2),
+                                           (Pos 3 3), (Pos 3 4), (Pos 3 1),
+                                           (Pos 3 0)
+                                          ]) ++
 
-            -- Knight at (1,1)
-            (movesFrom (1,1) [(0,3), (2,3), (3,0)]) ++
+               -- Knight at (1 1)
+               (normalMovesFrom (Pos 1 1) [(Pos 0 3), (Pos 2 3), (Pos 3 0)]) ++
 
-            -- Knight at (7,5)
-            (movesFrom (7,5) [(5,6), (5,4), (6,3)]) ++
+               -- Knight at (7 5)
+               (normalMovesFrom (Pos 7 5) [(Pos 5 6), (Pos 5 4), (Pos 6 3)]) ++
 
-            -- Knight at (6,4)
-            (movesFrom (6,4) [(4,3), (5,6), (7,6), (7,2), (5,2)]) ++
+               -- Knight at (6 4)
+               (normalMovesFrom (Pos 6 4) [(Pos 4 3), (Pos 5 6), (Pos 7 6),
+                                           (Pos 7 2), (Pos 5 2)
+                                          ]) ++
 
-            -- Pawn at (7,1)
-            promoteAt (Pos 7 1)
+               -- Pawn at (7 1)
+               promotesAt (Pos 7 1)
 
 prop_fixedBoardAllKindsPreventMove :: Property
 prop_fixedBoardAllKindsPreventMove = verifyMoves moves White board
@@ -98,7 +111,7 @@ prop_fixedBoardAllKindsPreventMove = verifyMoves moves White board
                   \6 ♜     ♙         6\n\
                   \7                 7\n\
                   \  0 1 2 3 4 5 6 7"
-    moves = movesFrom (5,3) [(6,4)]
+    moves = normalMovesFrom (Pos 5 3) [(Pos 6 4)]
 
 prop_fixedBoardMovesAwayIfIsChecked :: Property
 prop_fixedBoardMovesAwayIfIsChecked = verifyMoves moves White board
@@ -113,7 +126,8 @@ prop_fixedBoardMovesAwayIfIsChecked = verifyMoves moves White board
                   \6                 6\n\
                   \7               ♚ 7\n\
                   \  0 1 2 3 4 5 6 7"
-    moves = movesFrom (5,3) [(4,2), (4,3), (4,4), (6,2), (6,3), (6,4)]
+    moves = normalMovesFrom (Pos 5 3) [(Pos 4 2), (Pos 4 3), (Pos 4 4),
+                                       (Pos 6 2), (Pos 6 3), (Pos 6 4)]
 
 prop_fixedBoardBlocksWithOther :: Property
 prop_fixedBoardBlocksWithOther = verifyMoves moves White board
@@ -128,7 +142,7 @@ prop_fixedBoardBlocksWithOther = verifyMoves moves White board
                   \6       ♙     ♜ ♝ 6\n\
                   \7               ♛ 7\n\
                   \  0 1 2 3 4 5 6 7"
-    moves = movesFrom (6,3) [(5,3)]
+    moves = normalMovesFrom (Pos 6 3) [(Pos 5 3)]
 
 prop_fixedBoardCapturesThreat :: Property
 prop_fixedBoardCapturesThreat = verifyMoves moves White board
@@ -143,21 +157,17 @@ prop_fixedBoardCapturesThreat = verifyMoves moves White board
                   \6             ♜ ♝ 6\n\
                   \7               ♛ 7\n\
                   \  0 1 2 3 4 5 6 7"
-    moves = movesFrom (0,5) [(5,0)]
+    moves = normalMovesFrom (Pos 0 5) [(Pos 5 0)]
 
 verifyMoves :: [Move] -> Color -> Board -> Property
 verifyMoves expMoves color board =
     MTL.verifyMoves expMoves color board CA.movesForColor
 
+normalMovesFrom :: Pos -> [Pos] -> [Move]
+normalMovesFrom src dsts = map (NormalMove src) dsts
 
-movesFrom :: (Int,Int) -> [(Int,Int)] -> [Move]
-movesFrom start ends = map (\end -> NormalMove (toPos start) (toPos end)) ends
-
-promoteAt :: Pos -> [Move]
-promoteAt p = [Promote p k | k <- [Rook, Bishop, Knight, Queen]]
-
-toPos :: (Int,Int) -> Pos
-toPos (row,col) = Pos row col
+promotesAt :: Pos -> [Move]
+promotesAt p = [Promote p k | k <- [Rook, Bishop, Knight, Queen]]
 
 --------------------------------------------------------------------------------
 -- Arbitrary boards
