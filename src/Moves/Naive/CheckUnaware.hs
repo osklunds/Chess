7566e9ac2b3daf8d5f49b72bcd33c28b253cb4b3
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2022-09-25 16:09:24 +0200

    Castling checkUnaware

--- src/Moves/Naive/CheckUnaware.hs
+++ src/Moves/Naive/CheckUnaware.hs
@@ -12,7 +12,7 @@ import Moves.Common
 
 
 movesF :: MovesFun
-movesF c b = normalMoves c b ++ promotes c b
+movesF c b = normalMoves c b ++ promotes c b ++ castlings c b
 
 normalMoves :: Color -> Board -> [Move]
 normalMoves c b = map f $  NM.movesF c b
@@ -25,3 +25,29 @@ promotes c b = [Promote p k | p <- withPawn, k <- [Rook, Bishop, Knight, Queen]]
         r = if c == White then 0 else 7
         ps = [Pos r c | c <- [0..7]]
         withPawn = [p | p <- ps, let atP = getB p b, isPawn atP, isColor c atP]
+
+castlings :: Color -> Board -> [Move]
+castlings c b = kingSideCastle c b ++ queenSideCastle c b
+
+kingSideCastle :: Color -> Board -> [Move]
+kingSideCastle c b
+    | getBL [Pos row col | col <- [4..7]] b == lane = [Castle c KingSide]
+    | otherwise                                     = []
+    where
+        row = homeRow c
+        lane = [Piece c King, Empty, Empty, Piece c Rook]
+
+queenSideCastle :: Color -> Board -> [Move]
+queenSideCastle c b
+    | getBL [Pos row col | col <- [0..4]] b == lane = [Castle c QueenSide]
+    | otherwise                                     = []
+    where
+        row = homeRow c
+        lane = [Piece c Rook, Empty, Empty, Empty, Piece c King]
+
+homeRow :: Color -> Int
+homeRow White = 7
+homeRow Black = 0
+
+getBL :: [Pos] -> Board -> [Square]
+getBL ps b = [getB p b | p <- ps]
--- src/Moves/Naive/CheckUnaware/Tests.hs
+++ src/Moves/Naive/CheckUnaware/Tests.hs
@@ -49,6 +49,25 @@ promotesAt :: [Pos] -> [Move]
 promotesAt ps = [Promote p k | p <- ps, k <- [Rook, Bishop, Knight, Queen]]
 
 --------------------------------------------------------------------------------
+-- Fixed boards for castling
+--------------------------------------------------------------------------------
+
+prop_fixedBoardCastlingBoth :: Property
+prop_fixedBoardCastlingBoth = verifyMoves expMoves White board
+    where
+        board = read  "  0 1 2 3 4 5 6 7  \n\
+                      \0         ♜     ♗ 0\n\
+                      \1 ♛ ♕ ♟   ♛       1\n\
+                      \2     ♕ ♘ ♔       2\n\
+                      \3   ♘   ♙       ♚ 3\n\
+                      \4 ♞         ♙   ♟ 4\n\
+                      \5         ♟ ♘ ♙   5\n\
+                      \6   ♕   ♖   ♟     6\n\
+                      \7 ♖       ♔     ♖ 7\n\
+                      \  0 1 2 3 4 5 6 7"
+        expMoves = [Castle White QueenSide, Castle White KingSide]
+
+--------------------------------------------------------------------------------
 -- Arbitrary boards
 --------------------------------------------------------------------------------
 
