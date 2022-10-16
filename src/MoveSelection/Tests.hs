23f861612303459c4750908cd9be68a3d74ff9a7
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2022-10-16 08:50:24 +0200

    prop_doNotCastleIfThreatened

--- src/MoveSelection/Tests.hs
+++ src/MoveSelection/Tests.hs
@@ -195,18 +195,26 @@ prop_doNotPromote2 = verifyMakesMove expMove board
 prop_castling :: Bool
 prop_castling = verifyMakesMove expMove board
     where
-        board = read  "  0 1 2 3 4 5 6 7  \n\
-                      \0 ♜       ♚   ♞   0\n\
-                      \1                 1\n\
-                      \2   ♙ ♙ ♔ ♙       2\n\
-                      \3 ♘   ♙   ♙       3\n\
-                      \4         ♙       4\n\
-                      \5 ♗   ♙           5\n\
-                      \6     ♘         ♞ 6\n\
-                      \7 ♟   ♖   ♘       7\n\
-                      \  0 1 2 3 4 5 6 7"
+        board = castleToWinBoard
         expMove = Castle Black QueenSide
 
+castleToWinBoard :: Board
+castleToWinBoard = read  "  0 1 2 3 4 5 6 7  \n\
+                         \0 ♜       ♚   ♞   0\n\
+                         \1                 1\n\
+                         \2   ♙ ♙ ♔ ♙       2\n\
+                         \3 ♘   ♙   ♙       3\n\
+                         \4         ♙     ♗ 4\n\
+                         \5 ♗   ♙           5\n\
+                         \6     ♘         ♞ 6\n\
+                         \7 ♟   ♖   ♘       7\n\
+                         \  0 1 2 3 4 5 6 7"
+
+prop_doNotCastleIfThreatened :: Bool
+prop_doNotCastleIfThreatened = verifyDoesNotMakeMove move board
+    where
+        board = setB (Pos 4 7) (Piece White Bishop) castleToWinBoard
+        move = Castle Black QueenSide
 
 
 -- TODO: Test move that needs two steps ahead thinking
@@ -247,6 +255,12 @@ verifyMakesOneOfMoves :: [Move] -> Board -> Bool
 verifyMakesOneOfMoves expMoves board = all pred depths
   where
     pred depth = makeMove depth board `elem` expMoves
+
+verifyDoesNotMakeMove :: Move -> Board -> Bool
+verifyDoesNotMakeMove move board = all pred depths
+    where
+        pred depth = makeMove depth board /= move
+
 
 return []
 runTests = $quickCheckAll
