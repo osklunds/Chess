40680b055c8b90c3b352c2ff63014ef9ddd7aa9f
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2023-06-09 20:27:32 +0200

    Make moves away if checked pass

--- src/Moves/Naive/CheckAware/Tests.hs
+++ src/Moves/Naive/CheckAware/Tests.hs
@@ -128,8 +128,8 @@ prop_fixedBoardMovesAwayIfIsChecked :: Property
 prop_fixedBoardMovesAwayIfIsChecked = verifyMoves moves White board
   where
     board = read  "  0 1 2 3 4 5 6 7  \n\
-                  \0       ♙         0\n\
-                  \1 ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟ 1\n\
+                  \0                 0\n\
+                  \1 ♟ ♟ ♟ ♙ ♟ ♟ ♟ ♟ 1\n\
                   \2                 2\n\
                   \3                 3\n\
                   \4                 4\n\
