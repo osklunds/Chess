0f60eb4a32f191650d2d1e250c510031963d28a3
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2023-06-09 20:35:08 +0200

    Make two fixed board castling tests pass

--- src/Moves/Naive/CheckAware/Tests.hs
+++ src/Moves/Naive/CheckAware/Tests.hs
@@ -188,7 +188,7 @@ prop_fixedBoardNoCastlingKingWouldPass :: Property
 prop_fixedBoardNoCastlingKingWouldPass = verifyCastlingMoves moves White board
     where
         board = read  "  0 1 2 3 4 5 6 7  \n\
-                      \0 ♟ ♜ ♚   ♟ ♖ ♘   0\n\
+                      \0   ♜ ♚     ♖ ♘   0\n\
                       \1 ♕   ♞ ♘ ♟ ♝ ♘   1\n\
                       \2       ♞ ♛       2\n\
                       \3   ♖ ♖   ♝     ♟ 3\n\
@@ -225,7 +225,7 @@ prop_fixedBoardNoCastlingDestAttacked = verifyCastlingMoves moves Black board
                       \4   ♖ ♛ ♝     ♝ ♔ 4\n\
                       \5 ♙     ♞         5\n\
                       \6   ♟   ♜ ♘ ♕     6\n\
-                      \7   ♖ ♜   ♗   ♙ ♜ 7\n\
+                      \7   ♖ ♜   ♗     ♜ 7\n\
                       \  0 1 2 3 4 5 6 7"
         moves = [Castle Black QueenSide]
 
