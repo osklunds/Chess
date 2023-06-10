b275b8455337f06d39ddb65e33fa8bbbfad80d8f
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2023-06-10 09:23:26 +0200

    prop_checkedMultipleTypesOfMoves

--- src/Moves/Naive/CheckAware/Tests.hs
+++ src/Moves/Naive/CheckAware/Tests.hs
@@ -227,6 +227,22 @@ prop_kingCanCaptureThreat = verifyMoves moves White board
                   \  0 1 2 3 4 5 6 7"
     moves = normalMovesFrom (Pos 5 7) [(Pos 5 6)]
 
+prop_checkedMultipleTypesOfMoves :: Property
+prop_checkedMultipleTypesOfMoves = verifyMoves moves White board
+  where
+    board = read  "  0 1 2 3 4 5 6 7  \n\
+                  \0 ♚   ♘     ♗     0\n\
+                  \1               ♙ 1\n\
+                  \2                 2\n\
+                  \3                 3\n\
+                  \4                 4\n\
+                  \5             ♜ ♔ 5\n\
+                  \6           ♙     6\n\
+                  \7                 7\n\
+                  \  0 1 2 3 4 5 6 7"
+    moves = (normalMovesFrom (Pos 5 7) [(Pos 5 6), (Pos 4 7), (Pos 6 7)]) ++
+            (normalMovesFrom (Pos 6 5) [(Pos 5 6)])
+
 prop_movesAreSubsetOfCheckUnawareMoves :: Color -> Board -> Bool
 prop_movesAreSubsetOfCheckUnawareMoves color board =
   moves `isSubsetOf` movesCheckUnaware
