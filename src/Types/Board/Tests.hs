fd1d6826ac7a775f0f13228b2a46d60cbb0a8a99
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-01-04 06:28:13 +0100

    prop_applyMoveWhiteKingSideCastle

--- src/Types/Board/Tests.hs
+++ src/Types/Board/Tests.hs
@@ -431,6 +431,36 @@ prop_applyMoveBlackQueenSideCastle = verifyBoardsEqual expBoardAfter actBoardAft
                              \  M       U     U"
         actBoardAfter = applyMove (Castle Black QueenSide) boardBefore
 
+prop_applyMoveWhiteKingSideCastle :: Property
+prop_applyMoveWhiteKingSideCastle = verifyBoardsEqual expBoardAfter actBoardAfter
+    where
+        boardBefore = read "  M       M     M  \n\
+                           \  0 1 2 3 4 5 6 7  \n\
+                           \0   ♕             0\n\
+                           \1   ♗     ♘ ♟ ♜   1\n\
+                           \2 ♙ ♕   ♚ ♞ ♕ ♛   2\n\
+                           \3 ♘   ♛ ♟     ♙ ♛ 3\n\
+                           \4         ♜     ♜ 4\n\
+                           \5 ♛         ♛   ♜ 5\n\
+                           \6   ♙ ♖   ♕ ♙     6\n\
+                           \7   ♘     ♔     ♖ 7\n\
+                           \  0 1 2 3 4 5 6 7  \n\
+                           \  M       U     U"
+
+        expBoardAfter = read "  M       M     M  \n\
+                             \  0 1 2 3 4 5 6 7  \n\
+                             \0   ♕             0\n\
+                             \1   ♗     ♘ ♟ ♜   1\n\
+                             \2 ♙ ♕   ♚ ♞ ♕ ♛   2\n\
+                             \3 ♘   ♛ ♟     ♙ ♛ 3\n\
+                             \4         ♜     ♜ 4\n\
+                             \5 ♛         ♛   ♜ 5\n\
+                             \6   ♙ ♖   ♕ ♙     6\n\
+                             \7   ♘       ♖ ♔   7\n\
+                             \  0 1 2 3 4 5 6 7  \n\
+                             \  M       M     M"
+        actBoardAfter = applyMove (Castle White KingSide) boardBefore
+
 --------------------------------------------------------------------------------
 -- Arbitrary
 --------------------------------------------------------------------------------
