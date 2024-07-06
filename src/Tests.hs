341977490373faa97c0d519c2606020255117488
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-07-06 12:06:59 +0200

    Remove GameResult from Tests

--- src/Tests.hs
+++ src/Tests.hs
@@ -6,7 +6,6 @@ import qualified Moves.Tests as MO
 import qualified Score.Tests as S
 import qualified MoveSelection.Tests as MS
 import qualified Optimize.Tests as O
-import qualified GameResult.Tests as GR
 import qualified Memoization.Tests as ME
 
 runTests :: IO Bool
@@ -16,7 +15,6 @@ runTests = do
   s  <- S.runTests
   ms <- MS.runTests
   o  <- O.runTests
-  gr <- GR.runTests
   me <- ME.runTests
 
-  return $ and [t,mo,s,ms,o,gr,me]
+  return $ and [t,mo,s,ms,o,me]
