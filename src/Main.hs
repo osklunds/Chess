5160cbffd88c2af3643464424747fe778a50b9a1
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-07-06 13:45:53 +0200

    Adjust Game and Cli for new Score return value

--- src/Main.hs
+++ src/Main.hs
@@ -8,14 +8,17 @@ main = start playAs startBoard
 playAs = White
 
 -- Change comments to change what's used
-startBoard = defaultBoard
--- startBoard = read  "  0 1 2 3 4 5 6 7  \n\
---                      \0             ♙   0\n\
---                      \1 ♔               1\n\
---                      \2                 2\n\
---                      \3                 3\n\
---                      \4                 4\n\
---                      \5           ♟ ♚   5\n\
---                      \6                 6\n\
---                      \7     ♟           7\n\
---                      \  0 1 2 3 4 5 6 7"
+-- startBoard = defaultBoard
+startBoard = read  "  U       U     U  \n\
+                  \  0 1 2 3 4 5 6 7  \n\
+                  \0   ♕           ♝ 0\n\
+                  \1   ♞           ♝ 1\n\
+                  \2   ♔[ ]  ♝       2\n\
+                  \3     ♜   ♙     ♙ 3\n\
+                  \4     ♛   ♖ ♟ ♗ ♚ 4\n\
+                  \5     ♖         ♜ 5\n\
+                  \6       ♘ ♞ ♟ ♝ ♟ 6\n\
+                  \7       ♜   ♞ ♖ ♝ 7\n\
+                  \  0 1 2 3 4 5 6 7  \n\
+                  \  U       M     U\n\
+                  \[Black]"
