ed656d56527e33bd11a13d224c49a2a472809827
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-07-13 15:28:56 +0200

    Optimize score speed

--- src/Score.hs
+++ src/Score.hs
@@ -53,13 +53,7 @@ calculateScore board
    | scoreSum < -100000 = minBound
    | otherwise      = scoreSum
    where
-       scores = [scoreForPos (Pos row col) board | row <- [0..7], col <- [0..7]]
-       scoreSum = sum scores
-       
-
-scoreForPos :: Pos -> Board -> Int
-scoreForPos pos board = materialValue (getB pos board) +
-                        pieceSquareTable (getB pos board) !! (rowOf pos) !! (colOf pos)
+       (scoreSum, []) = foldB foldFunction (0, allPieceSquareTables) board
 
 materialValue :: Square -> Int
 materialValue (Piece White Pawn) = 100
@@ -71,58 +65,98 @@ materialValue (Piece White King) = 200000
 materialValue (Piece Black p) = -(materialValue $ Piece White p)
 materialValue Empty = 0
 
-pieceSquareTable :: Square -> [[Int]]
-pieceSquareTable (Piece White Pawn) =   [[ u,  u,  u,  u,  u,  u,  u,  u],
-                                         [50, 50, 50, 50, 50, 50, 50, 50],
-                                         [10, 10, 20, 30, 30, 20, 10, 10],
-                                         [ 5,  5, 10, 25, 25, 10,  5,  5],
-                                         [ 0,  0,  0, 20, 20,  0,  0,  0],
-                                         [ 5, -5,-10,  0,  0,-10, -5,  5],
-                                         [ 5, 10, 10,-20,-20, 10, 10,  5],
-                                         [ u,  u,  u,  u,  u,  u,  u,  u]]
-pieceSquareTable (Piece White Knight) = [[-50,-40,-30,-30,-30,-30,-40,-50],
-                                         [-40,-20,  0,  0,  0,  0,-20,-40],
-                                         [-30,  0, 10, 15, 15, 10,  0,-30],
-                                         [-30,  5, 15, 20, 20, 15,  5,-30],
-                                         [-30,  0, 15, 20, 20, 15,  0,-30],
-                                         [-30,  5, 10, 15, 15, 10,  5,-30],
-                                         [-40,-20,  0,  5,  5,  0,-20,-40],
-                                         [-50,-40,-30,-30,-30,-30,-40,-50]]
-pieceSquareTable (Piece White Bishop) = [[-20,-10,-10,-10,-10,-10,-10,-20],
-                                         [-10,  0,  0,  0,  0,  0,  0,-10],
-                                         [-10,  0,  5, 10, 10,  5,  0,-10],
-                                         [-10,  5,  5, 10, 10,  5,  5,-10],
-                                         [-10,  0, 10, 10, 10, 10,  0,-10],
-                                         [-10, 10, 10, 10, 10, 10, 10,-10],
-                                         [-10,  5,  0,  0,  0,  0,  5,-10],
-                                         [-20,-10,-10,-10,-10,-10,-10,-20]]
-pieceSquareTable (Piece White Rook) =   [[ 0,  0,  0,  0,  0,  0,  0,  0],
-                                         [ 5, 10, 10, 10, 10, 10, 10,  5],
-                                         [-5,  0,  0,  0,  0,  0,  0, -5],
-                                         [-5,  0,  0,  0,  0,  0,  0, -5],
-                                         [-5,  0,  0,  0,  0,  0,  0, -5],
-                                         [-5,  0,  0,  0,  0,  0,  0, -5],
-                                         [-5,  0,  0,  0,  0,  0,  0, -5],
-                                         [ 0,  0,  0,  5,  5,  0,  0,  0]]
-pieceSquareTable (Piece White Queen) =   [[-20,-10,-10, -5, -5,-10,-10,-20],
-                                         [-10,  0,  0,  0,  0,  0,  0,-10],
-                                         [-10,  0,  5,  5,  5,  5,  0,-10],
-                                         [ -5,  0,  5,  5,  5,  5,  0, -5],
-                                         [  0,  0,  5,  5,  5,  5,  0, -5],
-                                         [-10,  5,  5,  5,  5,  5,  0,-10],
-                                         [-10,  0,  5,  0,  0,  0,  0,-10],
-                                         [-20,-10,-10, -5, -5,-10,-10,-20]]
-pieceSquareTable (Piece White King) =   [[-30,-40,-40,-50,-50,-40,-40,-30],
-                                         [-30,-40,-40,-50,-50,-40,-40,-30],
-                                         [-30,-40,-40,-50,-50,-40,-40,-30],
-                                         [-30,-40,-40,-50,-50,-40,-40,-30],
-                                         [-20,-30,-30,-40,-40,-30,-30,-20],
-                                         [-10,-20,-20,-20,-20,-20,-20,-10],
-                                         [ 20, 20,  0,  0,  0,  0, 20, 20],
-                                         [ 20, 30, 10,  0,  0, 10, 30, 20]]
-pieceSquareTable (Piece Black p) = map (map (* (-1))) $ reverse $
+foldFunction :: (Int, [((Int,Int,Int,Int,Int,Int), (Int,Int,Int,Int,Int,Int), Int)]) ->
+       Square ->
+       (Int, [((Int,Int,Int,Int,Int,Int), (Int,Int,Int,Int,Int,Int), Int)])
+foldFunction (accScore, (((wp, wn, wb, wr, wq, wk), (bp, bn, bb, br, bq, bk), e)):rest)
+    square = (accScore+pstScore+materialScore, rest)
+    where
+        pstScore = case square of
+                       (Piece White Pawn)   -> wp
+                       (Piece White Knight) -> wn
+                       (Piece White Bishop) -> wb
+                       (Piece White Rook)   -> wr
+                       (Piece White Queen)  -> wq
+                       (Piece White King)   -> wk
+                       (Piece Black Pawn)   -> bp
+                       (Piece Black Knight) -> bn
+                       (Piece Black Bishop) -> bb
+                       (Piece Black Rook)   -> br
+                       (Piece Black Queen)  -> bq
+                       (Piece Black King)   -> bk
+                       Empty                -> e
+        materialScore = materialValue square
+
+allPieceSquareTables = zip3 whitePieceSquareTables
+                            blackPieceSquareTables
+                            (pieceSquareTable Empty)
+
+whitePieceSquareTables = zip6 (pieceSquareTable (Piece White Pawn))
+                              (pieceSquareTable (Piece White Knight))
+                              (pieceSquareTable (Piece White Bishop))
+                              (pieceSquareTable (Piece White Rook))
+                              (pieceSquareTable (Piece White Queen))
+                              (pieceSquareTable (Piece White King))
+
+blackPieceSquareTables = zip6 (pieceSquareTable (Piece Black Pawn))
+                              (pieceSquareTable (Piece Black Knight))
+                              (pieceSquareTable (Piece Black Bishop))
+                              (pieceSquareTable (Piece Black Rook))
+                              (pieceSquareTable (Piece Black Queen))
+                              (pieceSquareTable (Piece Black King))
+
+pieceSquareTable :: Square -> [Int]
+pieceSquareTable (Piece White Pawn) =   [ u,  u,  u,  u,  u,  u,  u,  u,
+                                         50, 50, 50, 50, 50, 50, 50, 50,
+                                         10, 10, 20, 30, 30, 20, 10, 10,
+                                          5,  5, 10, 25, 25, 10,  5,  5,
+                                          0,  0,  0, 20, 20,  0,  0,  0,
+                                          5, -5,-10,  0,  0,-10, -5,  5,
+                                          5, 10, 10,-20,-20, 10, 10,  5,
+                                          u,  u,  u,  u,  u,  u,  u,  u]
+pieceSquareTable (Piece White Knight) = [-50,-40,-30,-30,-30,-30,-40,-50,
+                                         -40,-20,  0,  0,  0,  0,-20,-40,
+                                         -30,  0, 10, 15, 15, 10,  0,-30,
+                                         -30,  5, 15, 20, 20, 15,  5,-30,
+                                         -30,  0, 15, 20, 20, 15,  0,-30,
+                                         -30,  5, 10, 15, 15, 10,  5,-30,
+                                         -40,-20,  0,  5,  5,  0,-20,-40,
+                                         -50,-40,-30,-30,-30,-30,-40,-50]
+pieceSquareTable (Piece White Bishop) = [-20,-10,-10,-10,-10,-10,-10,-20,
+                                         -10,  0,  0,  0,  0,  0,  0,-10,
+                                         -10,  0,  5, 10, 10,  5,  0,-10,
+                                         -10,  5,  5, 10, 10,  5,  5,-10,
+                                         -10,  0, 10, 10, 10, 10,  0,-10,
+                                         -10, 10, 10, 10, 10, 10, 10,-10,
+                                         -10,  5,  0,  0,  0,  0,  5,-10,
+                                         -20,-10,-10,-10,-10,-10,-10,-20]
+pieceSquareTable (Piece White Rook) =   [ 0,  0,  0,  0,  0,  0,  0,  0,
+                                          5, 10, 10, 10, 10, 10, 10,  5,
+                                         -5,  0,  0,  0,  0,  0,  0, -5,
+                                         -5,  0,  0,  0,  0,  0,  0, -5,
+                                         -5,  0,  0,  0,  0,  0,  0, -5,
+                                         -5,  0,  0,  0,  0,  0,  0, -5,
+                                         -5,  0,  0,  0,  0,  0,  0, -5,
+                                          0,  0,  0,  5,  5,  0,  0,  0]
+pieceSquareTable (Piece White Queen) =   [-20,-10,-10, -5, -5,-10,-10,-20,
+                                         -10,  0,  0,  0,  0,  0,  0,-10,
+                                         -10,  0,  5,  5,  5,  5,  0,-10,
+                                          -5,  0,  5,  5,  5,  5,  0, -5,
+                                           0,  0,  5,  5,  5,  5,  0, -5,
+                                         -10,  5,  5,  5,  5,  5,  0,-10,
+                                         -10,  0,  5,  0,  0,  0,  0,-10,
+                                         -20,-10,-10, -5, -5,-10,-10,-20]
+pieceSquareTable (Piece White King) =   [-30,-40,-40,-50,-50,-40,-40,-30,
+                                         -30,-40,-40,-50,-50,-40,-40,-30,
+                                         -30,-40,-40,-50,-50,-40,-40,-30,
+                                         -30,-40,-40,-50,-50,-40,-40,-30,
+                                         -20,-30,-30,-40,-40,-30,-30,-20,
+                                         -10,-20,-20,-20,-20,-20,-20,-10,
+                                          20, 20,  0,  0,  0,  0, 20, 20,
+                                          20, 30, 10,  0,  0, 10, 30, 20]
+pieceSquareTable (Piece Black p) = map negate $ reverse $
                                    pieceSquareTable $ Piece White p
-pieceSquareTable Empty = replicate 8 $ replicate 8 0
+pieceSquareTable Empty = replicate 64 0
     
 u = undefined                                   
 
